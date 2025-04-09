cfb1bf3f38a77a712f28f2c3932650f7befd51ae
aman <amanajaz990@gmail.com>
2025-04-09 14:11:46 +0530

    first

---
+++ src/main/java/com/email_package/QuickReplyAiApplication.java
@@ -0,0 +1,13 @@
+package com.email_package;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickReplyAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickReplyAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/email_package/QuickReplyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email_package;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickReplyAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
