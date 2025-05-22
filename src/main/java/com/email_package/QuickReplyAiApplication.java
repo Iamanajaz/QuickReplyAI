2925082e795fe60696d68bb098110a343507988f
aman <amanajaz990@gmail.com>
2025-05-22 13:05:52 +0530

    db update

--- src/main/java/com/email_package/QuickReplyAiApplication.java
+++ src/main/java/com/email_package/QuickReplyAiApplication.java
@@ -1,12 +1,16 @@
 package com.email_package;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.email_package", "app"})
 public class QuickReplyAiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(QuickReplyAiApplication.class, args);
 	}
 
